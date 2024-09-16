3eae5aa0909970ba4e1787849138427d5fbc1581
VINICIUS DA LUZ LIMA CABRAL PINTO <202402159216@alunos.ibmec.edu.br>
2024-09-16 10:57:43 -0300

    alteração js

---
+++ Aula 5/teste.js
@@ -0,0 +1,20 @@
+function func1(x, y){
+    return x + y
+  }
+  
+  const func2 = function(a, b){
+    return a * b;
+  }
+  
+  const func3 = entrada => {
+    return entrada + " é bonito pra dedeu"
+  }
+  
+  
+  const minha_lista = [25, 'casa', [34, 12], 9.5]
+  
+  const func4 = function(a){
+    console.log (a);
+  }
+  
+  minha_lista.forEach(func4)
